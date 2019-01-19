f3e0926ff08bf184fc3320eba6296ff4b3439f8d
Camden Taylor <camdentaylor1992@gmail.com>
2019-01-18 22:17:39 -0700

    Added delete functionality and more styling to main page. This is now a full crud app.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const expressSanitizer = require('express-sanitizer');
 
 //app config
 const app = express();
@@ -10,6 +12,8 @@ mongoose.connect('mongodb://localhost/restful_music_blog')
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 //Mongoose model config
 const postSchema = new mongoose.Schema({
@@ -53,6 +57,7 @@ app.get('/posts/new', (req, res) => {
 
 //Create route
 app.post('/posts', (req, res) => {
+    req.body.post.body = req.sanitize(req.body.post.body);
     Post.create(req.body.post, (err, newPost) => {
         if(err) {
             res.render('new');
@@ -70,6 +75,40 @@ app.get('/posts/:id', (req, res) => {
             res.redirect('/posts');
         } else {
             res.render('show', {post: foundPost});
+        }
+    })
+})
+
+//Edit route
+app.get('/posts/:id/edit', (req, res) => {
+    Post.findById(req.params.id, (err, foundPost) => {
+        if(err) {
+            res.redirect('/posts')
+        } else {
+            res.render('edit', {post: foundPost})
+        }
+    })
+});
+
+//Update route
+app.put('/posts/:id', (req, res) => {
+    req.body.post.body = req.sanitize(req.body.post.body);
+    Post.findByIdAndUpdate(req.params.id, req.body.post, (err, updatedPost) => {
+        if(err) {
+            res.redirect('/posts');
+        } else {
+            res.redirect(`/posts/${req.params.id}`);
+        }
+    });
+});
+
+//Destroy route
+app.delete('/posts/:id', (req, res) => {
+    Post.findByIdAndRemove(req.params.id, (err) => {
+        if(err){
+            res.redirect('/posts');
+        } else {
+            res.redirect('/posts')
         }
     })
 })
